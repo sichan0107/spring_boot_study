fab94645c968745184743b1fd963590470957a75
scc0103 <thdtlcks369@gmail.com>
2020-07-24 01:58:29 +0900

    CRUD 끝

--- board/src/main/java/board/contoller/BoardController.java
+++ board/src/main/java/board/contoller/BoardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.dto.BoardDto;
@@ -37,7 +38,29 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/board/openBoardDetail.do")
-	public ModelAndView openBoardDetail
+	public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception{
+		ModelAndView mv = new ModelAndView("/board/boardDetail");
+		BoardDto board = boardService.selectBoardDetail(boardIdx);
+		
+		mv.addObject("board", board);
+		return mv;
+	}
+	
+	@RequestMapping("/board/updateBoard.do")
+	public String updateBoard(BoardDto board) throws Exception{
+		boardService.updateBoard(board);
+		return "redirect:/board/openBoardList.do";
+	}
+	
+	@RequestMapping("/board/deleteBoard.do")
+	public String deleteBoard(int boardIdx) throws Exception{
+		boardService.deleteBoard(boardIdx);
+		return "redirect:/board/openBoardList.do";
+	}
+	
+	
+	
+	
 	
 	
 	
--- board/src/main/java/board/service/BoardService.java
+++ board/src/main/java/board/service/BoardService.java
@@ -13,5 +13,10 @@ public interface BoardService {
 	
 	void insertBoard(BoardDto board) throws Exception;
 	
+	BoardDto selectBoardDetail(int boardIdx) throws Exception;
+	
+	void updateBoard(BoardDto board) throws Exception;
+		
+	void deleteBoard(int boardIdx) throws Exception;
 	
 }
